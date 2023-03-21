7a9ad345b6d7469630ef4ac0ebca012c4ec4af74
edmundchong <edmund.w.chong@gmail.com>
2023-03-21 22:43:03 +0000

    aggregating and basic plotting

---
+++ containers.py
@@ -0,0 +1,18 @@
+"""
+data containers for bonsai event ping data
+"""
+
+
+class Experiment:
+    def __init__(self):
+        self.anim = {} #container for AnimalData
+        self.notes = {} #make note of any significant events e.g. food restriction
+        self.data_root = ''
+
+class AnimalData:
+    def __init__(self, name):
+        self.name = name
+        self.subsess_paths = {}
+        self.subsess = {}
+
+
---
+++ swm/touch.py
@@ -0,0 +1,54 @@
+"""
+event parser for pSWM touchscreen setup
+"""
+
+COLNAMES = ['TrialNum', 'FixationDur', 'RespError_cuefrac', 'Cue_D',
+            'CueRel1_x', 'CueRel1_y', 'CueRel2_x', 'CueRel2_y',
+            'Cue1_x', 'Cue1_y', 'Cue2_x', 'Cue2_y',
+            'anchor1_x', 'anchor1_y', 'anchor2_x', 'anchor2_y',
+            ]
+
+import pandas as pd
+from ..import general as genparse
+import pyfun.bamboo as boo
+
+
+
+def trial_summary(df_trial):
+    """
+    df_trial: df containing all events restricted to a single trial.
+    return crucial trial info as a single row
+    crucial trial info differs across experiments
+    """
+
+    fixated = boo.slice(df_trial, {'Value': ['FixationCompleted']}, '+')  # index where InTimerCompleted
+
+    if len(fixated) == 0:  # TODO account for trials where fixation did not complete
+        return None
+
+    fixated_row = fixated.index[0]
+
+
+    row_holder = pd.DataFrame({'val': None}, index=COLNAMES)
+
+    row_holder.loc['TrialNum', 'val'] = df_trial['TrialNum'].iloc[0]
+
+    for param in ['FixationDur', 'RespError_cuefrac', 'Cue_D']:
+        row_holder.loc[param, 'val'] = genparse.get_trial_param(df_trial, param, dtype='float')
+
+    for param, prefix in zip(['Cue_xy', 'anchor_xy', 'Cue_xyRel'],
+                             ['Cue'   , 'anchor',    'CueRel']):
+
+        param_prepost = genparse.closest_to_row(df_trial, param, fixated_row)
+
+        # TODO: can improve for efficiency
+        for i, v in enumerate(param_prepost):
+            if v is None:
+                param_prepost[i] = '(nan,nan)'
+
+        output = genparse.format_prepost_xy(param_prepost, prefix)
+
+        for k, v in output.items():
+            row_holder.loc[k, 'val'] = v
+
+    return row_holder
